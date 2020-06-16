106eb9d45cc0ac6645351f2751e1bdfaba5b9b70
anst <starininandrey@gmail.com>
2020-06-16 21:59:31 +0300

    Init

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Authorization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Authorization_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO
+        }
+
+        private void button_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            input_Login.Clear();
+            input_Password.Clear();
+        }
+
+        private void button_Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            var res = MessageBox.Show("Вы уверены, что хотите закрыть окно авторизации?", "Подтвердите действие", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (res == MessageBoxResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
