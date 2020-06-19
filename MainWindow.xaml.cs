8c11bf4cced860a1d6320744b599b5145eadadb7
anst <starininandrey@gmail.com>
2020-06-19 21:54:12 +0300

    Add signUp, password check

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,15 +20,34 @@ namespace Authorization
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string login = "login";
+        private string password = "123";
+        
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void button_Authorization_Click(object sender, RoutedEventArgs e)
+        {
+            if (input_Login.Text == login && input_Password.Password == password)
+            {
+                MessageBox.Show("Авторизация прошла успешно", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Вы ввели неверный логин или пароль.", "Предупреждение", MessageBoxButton.OK, MessageBoxImage.Warning);
+                input_Login.Clear();
+                input_Password.Clear();
+            }
+        }
+
         private void button_Clear_Click(object sender, RoutedEventArgs e)
         {
             input_Login.Clear();
             input_Password.Clear();
         }
+
         private void button_Cancel_Click(object sender, RoutedEventArgs e)
         {
             var res = MessageBox.Show("Вы уверены, что хотите закрыть окно авторизации?", "Подтвердите действие", MessageBoxButton.YesNo, MessageBoxImage.Question);
@@ -38,19 +56,17 @@ namespace Authorization
                 Close();
             }
         }
-        //функция входа
-        private void button_Authorization_Click(object sender, RoutedEventArgs e)
-        {
-            //TO-DO
-        }
-        //функция открытия регистрации
-        private void button_Regin_Click(object sender, RoutedEventArgs e)
-        {
-            Registration registration = new Registration();
-            registration.Show();
 
-            //главное окно для окна регистрации
-            //window1.Owner = this;
+        private void hyper_SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            SignUp signUp = new SignUp();
+            signUp.Show();
+            Close();
+        }
+
+        private void hyper_RestorePassword_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
