fc0c5cacfed70d2157b40c8028039442dc181b08
anst <starininandrey@gmail.com>
2020-06-30 16:09:42 +0300

    Add connect to DB

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
+using MySql.Data.MySqlClient;
 using static Logging.LogToFile;
 
 namespace Authorization
@@ -7,19 +9,49 @@ namespace Authorization
     public partial class MainWindow : Window
     {
         //TODO Значение переменных из БД
-        private string login = "login";
-        private string password = "123";
-        
+        private string login;
+        private string password;
+        private MySqlConnection connection;
+
+        private const string host = "mysql11.hostland.ru";
+        private const string database = "host1323541_suptest2";
+        private const string port = "3306";
+        private const string username = "host1323541_itstep";
+        private const string pass = "269f43dc";
+        private const string ConnString = "Server=" + host + ";Database=" + database + ";port=" + port + ";User Id=" + username + ";password=" + pass;
+
         public MainWindow()
         {
             InitializeComponent();
             Loaded += (sender, args) => Log("info.log", "INFO", "Окно авторизации загружено");
             Closed += (sender, args) => Log("info.log", "INFO", "Окно авторизации закрылось");
+            
+            connection = new MySqlConnection(ConnString);
+            connection.Open();
         }
 
         private void ButtonAuthorization_Click(object sender, RoutedEventArgs e)
         {
-            if (InputLogin.Text == login && InputPassword.Password == password)
+            var checkAccount = false;
+            const string sql = "SELECT login, pass FROM Account";
+            var command = new MySqlCommand {Connection = connection, CommandText = sql};
+            using var result = command.ExecuteReader();
+            while (result.Read())
+            {
+                login = result.GetString(0);
+                password = result.GetString(1);
+                
+                if (InputLogin.Text == login && InputPassword.Password == password)
+                {
+                    checkAccount = true;
+                    break;
+                }
+                else
+                {
+                    checkAccount = false;
+                }
+            }
+            if (checkAccount)
             {
                 MessageBox.Show("Авторизация прошла успешно", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
             }
